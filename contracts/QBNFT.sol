980db3dd7f08d820f560abe9fcee47900eeae17b
Vaibhav Khanna <vaibhavkhanna78@gmail.com>
2022-02-16 21:35:03 +0530

    minor fixes in nft contract

--- contracts/QBNFT.sol
+++ contracts/QBNFT.sol
@@ -4,25 +4,27 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 contract QuantumNFT is ERC721 {
-    address public _owner;
+
+    address immutable public _owner;
     mapping(uint256 => string) private _tokenURIs;
+    uint256 public _nftPrice;
 
     constructor(address owner_) ERC721("Quantum-Bridge-NFT", "QBNFT") {
         _owner = owner_;
     }
 
-    modifier OnlySystem() {
-        require(msg.sender == _owner);
+    modifier onlySystem() {
+        require(msg.sender == _owner, "illegal-caller");
         _;
     }
 
-    event logUserMint(
+    event UserMintLog (
         address indexed user,
         address indexed owner,
         string indexed tokenURI_
     );
 
-    event LogSystemMint(
+    event SystemMintLog (
         address indexed user,
         address indexed owner,
         uint256 indexed tokenId_
@@ -32,14 +34,15 @@ contract QuantumNFT is ERC721 {
         address to_,
         string memory tokenURI,
         uint256 tokenId_
-    ) public OnlySystem {
+    ) public onlySystem {
         _mint(to_, tokenId_);
         _setTokenURI(tokenId_, tokenURI);
-        emit LogSystemMint(msg.sender, to_, tokenId_);
+        emit SystemMintLog(msg.sender, to_, tokenId_);
     }
 
-    function userMint(address to_, string memory tokenURI_) public {
-        emit logUserMint(msg.sender, to_, tokenURI_);
+    function userMint(address to_, string memory tokenURI_) public payable {
+        require (msg.value >= _nftPrice, "less-amount-paid");
+        emit UserMintLog(msg.sender, to_, tokenURI_);
     }
 
     function _setTokenURI(uint256 tokenId, string memory _tokenURI)
@@ -52,4 +55,5 @@ contract QuantumNFT is ERC721 {
         );
         _tokenURIs[tokenId] = _tokenURI;
     }
+
 }
