d0a3d1914d437c68321dda02d4e8e2bf6f3ba72a
pradyuman-verma <pradyumnverma27@gmail.com>
2022-02-16 16:44:37 +0530

    updated script

--- scripts/Sequencer/sequencer.js
+++ scripts/Sequencer/sequencer.js
@@ -1,23 +1,42 @@
+const { expect } = require("chai");
 const hre = require("hardhat");
+const erc721Abi = require("../constants/erc721.json");
 const { ethers } = hre;
 
 const providerKovan = new ethers.providers.JsonRpcProvider(
   "https://eth-kovan.alchemyapi.io/v2/zHQ9LKzVTUmjFfzQvl4SpgJVdFX8eNAi"
 );
 
-const contractAddress = "";
-const contractABI = [];
-const contractInstance = new ethers.Contract(
-  contractAddress,
-  contractABI,
-  providerKovan
-);
+let nonce = 0;
+const idOwnerMap = new Map();
+// deploying for testing
+{
+  const [signer, owner] = ethers.getSigners();
 
-const filter = contractInstance.filter.Eventname();
+  const Connector = await ethers.getContractFactory("Quantum");
+  const contractInstance = await Connector.deploy(owner.address, owner.address);
+  await contractInstance.deployed();
+}
 
-contractInstance.on(
-  filter,
-  (indexed_params = {
-    // validate and if everthing is okay, call validators
-  })
-);
+// const contractAddress = "";
+// const contractABI = [];
+// const contractInstance = new ethers.Contract(
+//   contractAddress,
+//   contractABI,
+//   providerKovan
+// );
+
+let filter = contractInstance.filter.lockNftLog();
+
+contractInstance.on(filter, (_from, _id, _nftAddr) => {
+  idOwnerMap.set(_id, _from);
+});
+
+filter = contractInstance.filter.migrateLog();
+
+contractInstance.on(filter, (_from, _to, _tokenId) => {
+  expect(await contractInstance._tokenIdToSender(_tokenId)).to.be.not.eq(
+    ethers.constants.AddressZero
+  );
+  // mint NFT on the target chain here: avax fuji
+});
