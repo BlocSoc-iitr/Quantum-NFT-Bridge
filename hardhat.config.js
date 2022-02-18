cc0cd97447b8b46c8b8a1b8931d8851f51c53c05
pradyuman-verma <pradyumnverma27@gmail.com>
2022-02-18 18:54:26 +0530

    updated sequncer

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,4 +2,13 @@ require("@nomiclabs/hardhat-waffle");
 
 module.exports = {
   solidity: "0.8.4",
+  networks: {
+    hardhat: {
+      url: "https://eth-kovan.alchemyapi.io/v2/zHQ9LKzVTUmjFfzQvl4SpgJVdFX8eNAi",
+    },
+    kovan: {
+      url: "https://eth-kovan.alchemyapi.io/v2/zHQ9LKzVTUmjFfzQvl4SpgJVdFX8eNAi",
+      accounts: [`0x${process.env.PRIVATE_KEY}`],
+    },
+  },
 };
--- scripts/Sequencer/sequencer.js
+++ scripts/Sequencer/sequencer.js
@@ -1,7 +1,9 @@
 const { expect } = require("chai");
 const hre = require("hardhat");
-const erc721Abi = require("../constants/erc721.json");
 const { ethers } = hre;
+
+import cron from "node-cron";
+const erc721Abi = require("../constants/erc721.json");
 
 const providerKovan = new ethers.providers.JsonRpcProvider(
   "https://eth-kovan.alchemyapi.io/v2/zHQ9LKzVTUmjFfzQvl4SpgJVdFX8eNAi"
@@ -11,39 +13,56 @@ const providerFuji = new ethers.providers.JsonRpcProvider(
   "https://api.avax-test.network/ext/bc/C/rpc"
 );
 
+const accountX = new ethers.Wallet(`0x${process.env.PRIVATE_KEY}`).connect(
+  providerKovan
+);
+
+const accountY = new ethers.Wallet(`0x${process.env.PRIVATE_KEY}`).connect(
+  providerFuji
+);
+
 let nonce = 0;
 const idOwnerMap = new Map();
+
+const contractAddress = "";
+const contractABI = [];
+const contractInstanceChainX = new ethers.Contract(
+  contractAddress,
+  contractABI,
+  providerKovan
+);
+
+const contractInstanceChainY = new ethers.Contract(
+  contractAddress,
+  contractABI,
+  providerFuji
+);
+
 // deploying for testing
-{
-  const [signer, owner] = ethers.getSigners();
+// {
+//   const [signer, owner] = ethers.getSigners();
+//   const Connector = await ethers.getContractFactory("Quantum");
+//   const contractInstanceChainX = await Connector.deploy(owner.address, owner.address);
+//   await contractInstanceChainX.deployed();
+// }
 
-  const Connector = await ethers.getContractFactory("Quantum");
-  const contractInstance = await Connector.deploy(owner.address, owner.address);
-  await contractInstance.deployed();
-}
+cron.schedule(`* * * * *`, async () => {
+  let filter = contractInstanceChainX.filter.lockNftLog();
 
-// const contractAddress = "";
-// const contractABI = [];
-// const contractInstance = new ethers.Contract(
-//   contractAddress,
-//   contractABI,
-//   providerKovan
-// );
+  contractInstanceChainX.on(filter, (_from, _id, _nftAddr) => {
+    idOwnerMap.set(_id, _from);
+  });
 
-let filter = contractInstance.filter.lockNftLog();
+  filter = contractInstanceChainX.filter.migrateLog();
 
-contractInstance.on(filter, (_from, _id, _nftAddr) => {
-  idOwnerMap.set(_id, _from);
-});
-
-filter = contractInstance.filter.migrateLog();
-
-contractInstance.on(filter, (_from, _to, _tokenId) => {
-  expect(await contractInstance._tokenIdToSender(_tokenId)).to.be.not.eq(
-    ethers.constants.AddressZero
-  );
-  expect(await contractInstance._tokenIdToSender(_tokenId)).to.be.eq(
-    idOwnerMap.get(_tokenId)
-  );
-  // mint NFT on the target chain here: avax fuji
+  contractInstanceChainX.on(filter, (_from, _to, _tokenId) => {
+    expect(
+      await contractInstanceChainX._tokenIdToSender(_tokenId)
+    ).to.be.not.eq(ethers.constants.AddressZero);
+    expect(await contractInstanceChainX._tokenIdToSender(_tokenId)).to.be.eq(
+      idOwnerMap.get(_tokenId)
+    );
+    // mint NFT on the target chain here: avax fuji
+    await contractInstanceChainY.connect(accountY).systemMint();
+  });
 });
